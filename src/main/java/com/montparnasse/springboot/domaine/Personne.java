a036e83c8361718570e567e371fc140acd3a5982
HUIOS <services.huios@gmail.com>
2020-01-03 12:11:44 +0100

    classe personne

---
+++ src/main/java/com/montparnasse/springboot/domaine/Personne.java
@@ -0,0 +1,51 @@
+package com.montparnasse.springboot.domaine;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Personne {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	private double salaire;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public double getSalaire() {
+		return salaire;
+	}
+	public void setSalaire(double salaire) {
+		this.salaire = salaire;
+	}
+	
+	
+}
