09a27300a69a7fdfba8b25d35b45b90013ebf831
HUIOS <services.huios@gmail.com>
2020-01-03 11:16:14 +0100

    commit initial

---
+++ src/main/java/com/montparnasse/springboot/SpringBootCollaboratifApplication.java
@@ -0,0 +1,13 @@
+package com.montparnasse.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCollaboratifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCollaboratifApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/montparnasse/springboot/domaine/package-info.java
@@ -0,0 +1,1 @@
+package com.montparnasse.springboot.domaine;
